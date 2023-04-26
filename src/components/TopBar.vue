69e6288b05b405ff6e2dab35a4d01c3bc234d7a6
NelloRossi <rossianiello97@gmail.com>
2023-04-26 12:30:43 +0200

    fixed topbar

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="topbar d-flex justify-content-between align-items-center p-2 text-white">
-        <div class="ms-5">
+    <div class="topbar d-flex-column d-sm-flex text-center justify-content-between align-items-center p-2 text-white">
+        <div class="ms-sm-5">
             <span>(001) 88451234 88455438</span>
         </div>
-        <div class="me-5">
+        <div class="me-sm-5">
             <i class="fa-brands fa-facebook-f"></i>
             <i class="fa-brands fa-twitter mx-4"></i>
             <i class="fa-brands fa-pinterest-p"></i>
