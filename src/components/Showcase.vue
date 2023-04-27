29dc70ac96374d0fea0992a98662553645911320
NelloRossi <rossianiello97@gmail.com>
2023-04-27 13:08:06 +0200

    fixed right div of showcase component

--- src/components/Showcase.vue
+++ src/components/Showcase.vue
@@ -1,12 +1,12 @@
 <template>
-    <section class="mx-3 d-flex flex-column flex-lg-row align-items-center">
+    <section class="mx-3 d-flex flex-column flex-lg-row align-items-center justify-content-between">
         <div class="left">
             <Divider/>
             <h2 class="fs-1 fw-bold my-5"> Our recent <span>web designs</span>  & some examples of past <span>projects</span> </h2>
             <Divider/>
         </div>
-        <div class="right d-flex align-self-end">
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind
+        <div class="right d-flex align-items-end h-100">
+            <p class="mt-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind
                 texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language
                 ocean.Separated they live in Bookmarksgrove.Far far away, behind the word mountains, far from the
                 countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.
@@ -38,7 +38,7 @@ import Divider from './Divider.vue';
     }
 }
 .right{
-    width: 55%;
+    width: 50%;
 }
 
 
