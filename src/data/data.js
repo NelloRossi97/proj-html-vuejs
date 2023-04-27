55d610e25bbce3428a2f0c4ee6f367255e3bb3b0
NelloRossi <rossianiello97@gmail.com>
2023-04-27 13:18:55 +0200

    added showcase data

--- src/data/data.js
+++ src/data/data.js
@@ -5,4 +5,47 @@ const contact = [
     'info@yourdomaimcom',
     '+99 (0) 101 0000 888'];
 
-export { navbar, quickLinks, contact };
+const showcase = [
+    {
+        title: 'The Basket of Flowers',
+        category: 'branding - marketing',
+        image: '/images/flowers.jpg'
+    },
+    {
+        title: 'A Famous Ferrys Wheel',
+        category: 'marketing',
+        image: '/images/panoramic-wheel.jpg'
+    },
+    {
+        title: 'Complementary Colors',
+        category: 'branding - marketing',
+        image: '/images/yellow-shoes.jpg'
+    },
+    {
+        title: 'Business Prestige',
+        category: 'branding',
+        image: '/images/desktop.jpg'
+    },
+    {
+        title: 'Colorful Origami Boats',
+        category: 'branding - marketing',
+        image: '/images/origami.jpg'
+    },
+    {
+        title: 'Modern Couch',
+        category: 'marketing',
+        image: '/images/sofa.jpg'
+    },
+    {
+        title: 'Hot Air Baloons',
+        category: 'branding - marketing',
+        image: '/images/air-baloon.jpg'
+    },
+    {
+        title: 'Make Difference',
+        category: 'branding',
+        image: '/images/blue-lemon.jpg'
+    }
+]
+
+export { navbar, quickLinks, contact, showcase };
