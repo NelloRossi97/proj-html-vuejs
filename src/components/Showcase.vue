d65f1d6daa75f11d22bb8bf0d8cb3bda51b7e834
NelloRossi <rossianiello97@gmail.com>
2023-04-27 13:52:43 +0200

    added showcase section

--- src/components/Showcase.vue
+++ src/components/Showcase.vue
@@ -1,25 +1,42 @@
 <template>
-    <section class="mx-3 d-flex flex-column flex-lg-row align-items-center justify-content-between">
-        <div class="left">
-            <Divider/>
-            <h2 class="fs-1 fw-bold my-5"> Our recent <span>web designs</span>  & some examples of past <span>projects</span> </h2>
-            <Divider/>
+    <section class="mx-3">
+        <div class="d-flex flex-column flex-lg-row align-items-center justify-content-between">
+            <div class="left">
+                <Divider/>
+                <h2 class="fs-1 fw-bold my-5"> Our recent <span>web designs</span>  & some examples of past <span>projects</span> </h2>
+                <Divider/>
+            </div>
+            <div class="right d-flex align-items-end h-100">
+                <p class="mt-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind
+                    texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language
+                    ocean.Separated they live in Bookmarksgrove.Far far away, behind the word mountains, far from the
+                    countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.
+                </p>
+            </div>
         </div>
-        <div class="right d-flex align-items-end h-100">
-            <p class="mt-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind
-                texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language
-                ocean.Separated they live in Bookmarksgrove.Far far away, behind the word mountains, far from the
-                countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.
-            </p>
+        
+        <div class="row w-100 my-5">
+            <ShowCaseCard v-for="item in showcase" :image="item.image" :title="item.title" :category="item.category"/>
+        </div>
+        <div class="text-center mb-5">
+            <ButtonPrimary :text="'read more'"/>
         </div>
     </section>
 </template>
 
 <script>
+import {showcase} from '../data/data'
 import Divider from './Divider.vue';
+import ShowCaseCard from './ShowCaseCard.vue';
+import ButtonPrimary from './ButtonPrimary.vue';
     export default {
     name: 'Showcase',
-        components:{Divider}
+    components: { Divider, ShowCaseCard, ButtonPrimary },
+    data() {
+        return {
+                showcase
+            }
+        }
     }
 </script>
 
---
+++ src/components/ShowcaseCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="card" class="col-12 col-sm-6 col-lg-3 mb-4 position-relative">
+        <img :src="image" :alt="title" class="img-fluid rounded-2">
+        <div class="position-absolute description text-white text-center d-flex flex-column justify-content-end rounded-2">
+            <h3 class="text-capitalize fw-bold">{{ title }}</h3>
+            <span class="text-uppercase mb-3">{{ category }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'ShowcaseCard',
+        props:['image', 'title', 'category']
+    }
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
+#card{
+    .description{
+        top: 0;
+        width: calc(100% - 1.5rem);
+        height: 100%;
+        background: $hover-bg;
+        opacity: 0%;
+        transition: 1s;
+    }
+    &:hover .description{
+        opacity: 85%;
+        cursor: pointer;
+    }
+}
+
+</style>
